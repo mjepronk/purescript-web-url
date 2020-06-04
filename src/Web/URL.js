222a43b96d6c2ad9a6454957900210e9a4cb9ac3
Matthias Pronk <git@masida.nl>
2020-06-04 21:51:25 +0200

    Added setters.

---
+++ src/Web/URL.js
@@ -0,0 +1,46 @@
+exports.mkURL = function(base) {
+    try {
+        return new URL(base);
+    } catch (TypeError) {
+        return undefined;
+    }
+};
+
+exports.mkURLRel = function(rel) {
+    return function(base) {
+        try {
+            return new URL(rel, base);
+        } catch (TypeError) {
+            return undefined;
+        }
+    }
+};
+
+exports.isUndefined = function(value) {
+    return value === undefined;
+};
+
+exports.toString     = function(url) { return url.toString(); };
+exports.hash         = function(url) { return url.hash; }
+exports.host         = function(url) { return url.host; }
+exports.hostname     = function(url) { return url.hostname; }
+exports.href         = function(url) { return url.href; }
+exports.origin       = function(url) { return url.origin; }
+exports.password     = function(url) { return url.password; }
+exports.pathname     = function(url) { return url.pathname; }
+exports.port         = function(url) { return url.port; }
+exports.protocol     = function(url) { return url.protocol; }
+exports.search       = function(url) { return url.search; }
+exports.searchParams = function(url) { return url.searchParams; }
+exports.username     = function(url) { return url.username; }
+
+exports.setHash     = function(v) { return function(url) { u = new URL(url); u.hash = v; return u; } }
+exports.setHost     = function(v) { return function(url) { u = new URL(url); u.host = v; return u; } }
+exports.setHostname = function(v) { return function(url) { u = new URL(url); u.hostname = v; return u; } }
+exports.setHref     = function(v) { return function(url) { u = new URL(url); u.href = v; return u; } }
+exports.setPassword = function(v) { return function(url) { u = new URL(url); u.password = v; return u; } }
+exports.setPathname = function(v) { return function(url) { u = new URL(url); u.pathname = v; return u; } }
+exports.setPort     = function(v) { return function(url) { u = new URL(url); u.port = v; return u; } }
+exports.setProtocol = function(v) { return function(url) { u = new URL(url); u.protocol = v; return u; } }
+exports.setSearch   = function(v) { return function(url) { u = new URL(url); u.search = v; return u; } }
+exports.setUsername = function(v) { return function(url) { u = new URL(url); u.username = v; return u; } }
--- src/Web/URL.purs
+++ src/Web/URL.purs
@@ -1,15 +1,44 @@
 module Web.URL
-  ( fromAbsolute
+  ( URL
+  , URLSearchParams
+  , fromAbsolute
   , fromRelative
-  , module Exports
+  , toString
+  , hash
+  , host
+  , hostname
+  , href
+  , origin
+  , password
+  , pathname
+  , port
+  , protocol
+  , search
+  , searchParams
+  , setHash
+  , setHost
+  , setHostname
+  , setHref
+  , setPassword
+  , setPathname
+  , setPort
+  , setProtocol
+  , setSearch
+  , setUsername
+  , username
   )
 where
 
+import Prelude
 import Data.Maybe (Maybe(..))
-import Web.URL.Foreign (URL, mkURL, mkURLRel, isUndefined)
-import Web.URL.Foreign (toString, hash, host, hostname, href, origin, password,
-    pathname, port, protocol, search, searchParams, username) as Exports
 
+
+foreign import data URL :: Type
+
+instance showURL :: Show URL where
+  show = toString
+
+foreign import data URLSearchParams :: Type
 
 -- | Construct a URL from a string containing an absolute URL.
 fromAbsolute :: String -> Maybe URL
@@ -21,3 +50,63 @@ fromAbsolute base = if isUndefined url then Nothing else Just url
 fromRelative :: String -> String -> Maybe URL
 fromRelative rel base = if isUndefined url then Nothing else Just url
     where url = mkURLRel rel base
+
+foreign import mkURL :: String -> URL
+foreign import mkURLRel :: String -> String -> URL
+foreign import isUndefined :: URL -> Boolean
+
+
+-- | Returns a String containing the whole URL.
+foreign import toString :: URL -> String
+
+-- | A String containing a '#' followed by the fragment identifier of the URL.
+foreign import hash :: URL -> String
+
+-- | A String containing the domain (that is the hostname) followed by (if a
+-- | port was specified) a ':' and the port of the URL.
+foreign import host :: URL -> String
+
+-- | A String containing the domain of the URL.
+foreign import hostname :: URL -> String
+
+-- | Returns a String containing the whole URL (synonym for toString).
+foreign import href :: URL -> String
+
+-- | Returns a String containing the origin of the URL, that is its scheme,
+-- | its domain and its port.
+foreign import origin :: URL -> String
+
+-- | A String containing the password specified before the domain name.
+foreign import password :: URL -> String
+
+-- | A String containing an initial '/' followed by the path of the URL.
+foreign import pathname :: URL -> String
+
+-- | A String containing the port number of the URL.
+foreign import port :: URL -> String
+
+-- | A String containing the protocol scheme of the URL, including the final ':'.
+foreign import protocol :: URL -> String
+
+-- | A String indicating the URL's parameter string; if any parameters are
+-- | provided, this string includes all of them, beginning with the leading '?'
+-- | character.
+foreign import search :: URL -> String
+
+-- | A URLSearchParams object which can be used to access the individual query
+-- | parameters found in search.
+foreign import searchParams :: URL -> URLSearchParams
+
+-- | A String containing the username specified before the domain name.
+foreign import username :: URL -> String
+
+foreign import setHash :: String -> URL -> URL
+foreign import setHost :: String -> URL -> URL
+foreign import setHostname :: String -> URL -> URL
+foreign import setHref :: String -> URL -> URL
+foreign import setPassword :: String -> URL -> URL
+foreign import setPathname :: String -> URL -> URL
+foreign import setPort :: String -> URL -> URL
+foreign import setProtocol :: String -> URL -> URL
+foreign import setSearch :: String -> URL -> URL
+foreign import setUsername :: String -> URL -> URL
